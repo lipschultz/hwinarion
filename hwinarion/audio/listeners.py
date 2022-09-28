5978f57f32c804633ab2d4cacee60412eceba527
Michael Lipschultz <michael.lipschultz@gmail.com>
2022-09-28 12:27:44 -0400

    Document listeners

--- hwinarion/audio/listeners.py
+++ hwinarion/audio/listeners.py
@@ -32,6 +32,10 @@ class BackgroundListener:
         self.queue = SimpleQueue()
 
     def start(self) -> None:
+        """
+        Start the background listener in a background thread.  Any audio samples recorded will be available in the
+        ``queue`` attribute.
+        """
         if self._thread is not None:
             raise ListenerRunningError()
         self._thread = threading.Thread(target=self._listen)
@@ -39,9 +43,14 @@ class BackgroundListener:
         self.__is_running = True
         self._thread.start()
 
-    def stop(self, timeout=None) -> bool:
+    def stop(self, timeout: Optional[int] = None) -> bool:
         """
-        Returns True if the listening thread stopped, False otherwise.
+        Stop the background listsener.
+
+        ``timeout`` indicates how long to wait when joining the background listener thread.  If ``None`` (default),
+        then don't wait.
+
+        Returns ``True`` if the listening thread stopped or ``timeout`` was ``None``, ``False`` otherwise.
         """
         self.__is_running = False
         self._thread.join(timeout)
@@ -51,14 +60,19 @@ class BackgroundListener:
 
     @property
     def is_listening(self) -> bool:
+        """
+        Return whether the listener is running.
+        """
         return self.__is_running
 
     def _listen(self):
+        """
+        The method that does the actual listening and adding the audio to a queue.
+        """
         while self.is_listening:
             try:
                 audio = self._listener.listen(**self._listener_kwargs)
                 self.queue.put(audio)
-                print(audio)
             except Exception as ex:
                 pass
 
@@ -68,27 +82,27 @@ class BaseListener:
         self.source = source
 
     def _determine_frame_state(self, latest_frame: AudioSample, all_frames: List[AnnotatedFrame]) -> FrameStateEnum:
+        """
+        Determine the state of the listener from the ``latest_frame`` that's recorded and all the frames that have been
+        recorded (except for the ``latest_frame``.
+        """
         raise NotImplementedError
 
     def _pre_process_individual_audio_sample(self, audio: AudioSample) -> AudioSample:
-        return audio
-
-    def _filter_audio_samples(self, all_frames: List[AnnotatedFrame]) -> List[AnnotatedFrame]:
-        previous_state = None
-        saved_frames = []
-        for frame in all_frames:
-            if frame.state == FrameStateEnum.LISTEN or previous_state == FrameStateEnum.LISTEN:
-                saved_frames.append(frame)
-            previous_state = frame.state
-        return saved_frames
-
-    def _join_audio_samples(self, all_frames: List[AnnotatedFrame]) -> AudioSample:
-        return AudioSample.from_iterable(frame.frame for frame in all_frames)
-
-    def _post_process_final_audio_sample(self, audio: AudioSample) -> AudioSample:
+        """
+        Apply any kind of filtering or modification of an audio sample before determining the frame state.  By default,
+        it just returns the audio sample unchanged.
+        """
         return audio
 
     def listen_frames(self, chunk_size: int = 2**14) -> List[AnnotatedFrame]:
+        """
+        Record and collect audio frames until the ``STOP`` frame state has been reached.  Return a list of all frames
+        recorded (including the ``STOP`` frame).
+
+        If the end of the source is reached, then listening will end regardless of what ``_determine_frame_state`` will
+        stop.
+        """
         all_frames = []
 
         frame = self.source.read(chunk_size)
@@ -104,9 +118,37 @@ class BaseListener:
         all_frames.append(AnnotatedFrame(frame, frame_state))
         return all_frames
 
+    def _filter_audio_samples(self, all_frames: List[AnnotatedFrame]) -> List[AnnotatedFrame]:
+        """
+        Modify/remove audio samples before they are merged into a single audio sample.
+
+        This method keeps all frames where the frame state was ``LISTEN`` or where the previous frame's state was
+        ``LISTEN``.
+        """
+        previous_state = None
+        saved_frames = []
+        for frame in all_frames:
+            if frame.state == FrameStateEnum.LISTEN or previous_state == FrameStateEnum.LISTEN:
+                saved_frames.append(frame)
+            previous_state = frame.state
+        return saved_frames
+
+    def _join_audio_samples(self, all_frames: List[AnnotatedFrame]) -> AudioSample:
+        """
+        Combine audio frames into a single audio sample.
+        """
+        return AudioSample.from_iterable(frame.frame for frame in all_frames)
+
+    def _post_process_final_audio_sample(self, audio: AudioSample) -> AudioSample:
+        """
+        Apply any kind of filtering or modification of the final audio sample.  By default, it just returns the audio
+        sample unchanged.
+        """
+        return audio
+
     def listen(self, chunk_size: int = 2**14) -> AudioSample:
         """
-        If the end of the source is reached, then listening will end regardless of what ``_determine_frame_state`` will return.
+        This method drives the listening action by calling the other methods and returning the final result.
         """
         all_frames = self.listen_frames(chunk_size)
         all_frames = self._filter_audio_samples(all_frames)
@@ -116,6 +158,9 @@ class BaseListener:
         return audio_sample
 
     def background_listen(self, chunk_size: int = 2**14) -> BackgroundListener:
+        """
+        Produce a background listener object that uses this listener.
+        """
         return BackgroundListener(self, listener_kwargs={'chunk_size': chunk_size})
 
 
