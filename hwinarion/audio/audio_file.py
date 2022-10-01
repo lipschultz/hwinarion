12fe55e19a3788f319773e488e899c2c8830d8f6
Michael Lipschultz <michael.lipschultz@gmail.com>
2022-10-01 10:19:46 -0400

    Support jumping to frame in audio file & Raise EOFError at end of file

--- hwinarion/audio/audio_file.py
+++ hwinarion/audio/audio_file.py
@@ -12,13 +12,26 @@ class AudioFile(BaseAudioSource):
         self._audio_data = AudioSegment.from_file(filepath)
         self.frame_index = 0
 
+    def __max_audio_frames(self) -> int:
+        return int(self._audio_data.frame_count())
+
+    def jump_to_frame(self, frame_number: int):
+        assert isinstance(frame_number, int) and 0 <= frame_number < self.__max_audio_frames()
+        self.frame_index = frame_number
+
+    def reset(self):
+        self.jump_to_frame(0)
+
     @property
     def frame_rate(self) -> int:
         return self._audio_data.frame_rate
 
     def read_pydub(self, n_frames: Optional[int]) -> AudioSegment:
+        if self.frame_index >= self.__max_audio_frames():
+            raise EOFError(f'Attempted to read past the end of the audio file.')
+
         if n_frames is None:
-            return self._audio_data
+            n_frames = self.__max_audio_frames() - self.frame_index
 
         assert isinstance(n_frames, int) and n_frames > 0, f'n_frames must be an integer greater than zero, got {n_frames!r}'
 
@@ -27,6 +40,11 @@ class AudioFile(BaseAudioSource):
         return read_data
 
     def read(self, n_frames: Optional[int]) -> AudioSample:
+        """
+        Read frames of audio from the file and return them in an ``AudioSample``.
+
+        If ``n_frames` is ``None``, read all remaining frames in the file.
+        """
         return AudioSample(self.read_pydub(n_frames))
 
 
